1743f1823ce127a477f7b4d90376a09d55431ccb
Odumz <odumz20@gmail.com>
2021-12-05 11:53:27 +0100

    userplan model, controller, route, policy, service update

--- src/models/user.ts
+++ src/models/user.ts
@@ -27,7 +27,40 @@ const UserSchema: Schema = new Schema(
         password: {
             type: String,
             trim: true,
-            required: true
+            minLength: 8
+        },
+        role: {
+            type: String,
+            enum: ['admin', 'clickar'],
+            default: 'clickar'
+        },
+        isClickar: {
+            type: Boolean,
+            default: 1
+        },
+        isAdmin: {
+            type: Boolean,
+            default: 0
+        },
+        isVerified: {
+            type: Boolean,
+            default: 0
+        },
+        profileImage: {
+            type: String,
+            minLength: 14
+        },
+        plan: {
+            type: Schema.Types.ObjectId,
+            ref: 'userplan'
+        },
+        isPushNotificationActive: {
+            type: Boolean,
+            default: 0
+        },
+        isSubscribed: {
+            type: Boolean,
+            default: 0
         }
     },
     {
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import clientRoute from './clientRoute';
 import providerRoute from './providerRoute';
 import authRoute from './authRoute';
+import userplanRoute from './userplanRoute';
 
 const router = express.Router();
 
 router.use('/clients', clientRoute); // use the client routes
 router.use('/providers', providerRoute); // use the provider routes
 router.use('/auth', authRoute); // use the auth routes
+router.use('/userplan', userplanRoute); // use the user plan routes
 
 export = router;

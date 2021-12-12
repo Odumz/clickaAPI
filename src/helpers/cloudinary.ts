5caa49ec2aeb08aa410181ae972c4b81ca568ca5
Odumz <odumz20@gmail.com>
2021-12-12 15:20:12 +0100

    cloudinary error fixed

--- src/helpers/cloudinary.ts
+++ src/helpers/cloudinary.ts
@@ -3,7 +3,7 @@ import cloudinary from 'cloudinary'
 const { CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET, CLOUDINARY_CLOUD_NAME } = process.env;
 
 // cloudinary config
-cloudinary.config({
+cloudinary.v2.config({
     cloud_name: CLOUDINARY_CLOUD_NAME,
     api_key: CLOUDINARY_API_KEY,
     api_secret: CLOUDINARY_API_SECRET

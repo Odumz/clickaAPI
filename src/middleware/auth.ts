5abf3d24d0586d1352d2efb83effc771acd4029d
Odumz <odumz20@gmail.com>
2021-12-12 15:14:50 +0100

    auth middleware has errors

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,49 +1,52 @@
-class Auth {
-    static async authenticateUser(req, res, next) {
-        if (!req.headers.authorization) {
-            return errorRes(next, 401, 'The authorization header is required');
-        }
+// import { NextFunction } from 'express';
+// import ApiError from '../helpers/ApiError';
+// import { verifyToken } from '../helpers/jwtServices'
+// class Auth {
+//     static async authenticateUser(req:Request, res:Response, next:NextFunction) {
+//         if (!req.headers) {
+//             throw new ApiError(401, 'The authorization header is required');
+//         }
 
-        let splitHeader = req.headers.authorization.split(' ');
-        // console.log(splitHeader);
-        if (splitHeader[0] !== 'Bearer') {
-            return errorRes(next, 401, 'wrong authorization format used...bearer <token> expected</token>');
-        }
+//         let splitHeader = req.headers.authorization.split(' '); // error with req.bearers....
+//         // console.log(splitHeader);
+//         if (splitHeader[0] !== 'Bearer') {
+//             throw new ApiError(401, 'wrong authorization format used...bearer <token> expected</token>');
+//         }
 
-        let token = splitHeader[1];
-        // console.log(token)
+//         let token = splitHeader[1];
+//         // console.log(token)
 
-        let decodedToken = verifyToken(token);
+//         let decodedToken = verifyToken(token);
 
-        if (!decodedToken) {
-            return errorRes(next, 401, 'user not found');
-        }
+//         if (!decodedToken) {
+//             throw new ApiError(401, 'user not found');
+//         }
 
-        // console.log("dectoken:", decodedToken)
-        req.user = decodedToken;
-        // console.log("req.user", req.user);
-        next();
-        // return decodedToken
-    }
+//         // console.log("dectoken:", decodedToken)
+//         req.user = decodedToken; // error with req.user
+//         // console.log("req.user", req.user);
+//         next();
+//         // return decodedToken
+//     }
 
-    static async isAdminUser(req, res, next) {
-        if (req.user.role !== 'admin') {
-            return rerrorRes(next, 401, 'this route is restricted to admin users');
-        }
-        return next();
-    }
+//     static async isAdminUser(req:Request, res:Response, next:NextFunction) {
+//         if (req.user.role !== 'admin') {  // error with req.user
+//             throw new ApiError(401, 'this route is restricted to admin users');
+//         }
+//         return next();
+//     }
 
-    static async isInvestor(req, res, next) {
-        // console.log(req.user);
-        let userRole = req.user.role;
-        // console.log(userRole);
-        if (req.user.role !== 'investor') {
-            if (req.user.role !== 'admin') {
-                return errorRes(next, 401, 'you are not allowed on this route');
-            }
-        }
-        return next();
-    }
-}
+//     static async isInvestor(req:Request, res:Response, next:NextFunction) {
+//         // console.log(req.user);
+//         let userRole = req.user.role; // error with req.user
+//         // console.log(userRole);
+//         if (req.user.role !== 'investor') { // error with req.user
+//             if (req.user.role !== 'admin') { // error with req.user
+//                 throw new ApiError(401, 'you are not allowed on this route');
+//             }
+//         }
+//         return next();
+//     }
+// }
 
-module.exports = Auth;
+// module.exports = Auth;
